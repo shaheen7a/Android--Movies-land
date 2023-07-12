96f51b585255a76ee33f42b323b7abf94e4ba452
shaheen7a <aousshaheen7@gmail.com>
2023-07-12 07:45:37 -0700

    Actor Details

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native"
 import { createStackNavigator } from '@react-navigation/stack';
 import { HomeScreen } from "../screens/HomeScreen.js";
 import MovieScreen from "../screens/MovieScreen.js";
+import PersonScreen from "../screens/PersonScreen.js";
 
 
 const Stack = createStackNavigator();
@@ -12,6 +13,7 @@ export const AppNavigation = () => {
       <Stack.Navigator>
         <Stack.Screen name="Home" options={{ headerShown: false }} component={HomeScreen} />
         <Stack.Screen name="Movie" options={{ headerShown: false }} component={MovieScreen} />
+        <Stack.Screen name="Person" options={{ headerShown: false }} component={PersonScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
---
+++ screens/PersonScreen.js
@@ -0,0 +1,112 @@
+import { View, Text, Dimensions, Image } from 'react-native'
+import React, { useState } from 'react'
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { styles } from '../theme';
+import { ChevronLeftIcon } from 'react-native-heroicons/outline';
+import { HeartIcon } from 'react-native-heroicons/solid';
+import MovieList from '../components/MovieList';
+
+
+var { width, height } = Dimensions.get('window');
+const verticalMargin = ' my-3';
+
+const PersonScreen = () => {
+  const { params: item } = useRoute();
+  const [isFavourite, toggleFavourite] = useState(false);
+  const navigation = useNavigation();
+  const [person, setPerson] = useState({});
+  const [personMovies, setPersonMovies] = useState([1, 2, 3, 4]);
+
+  return (
+    <ScrollView
+      className="flex-1 bg-neutral-900"
+      contentContainerStyle={{ paddingBottom: 20 }}
+    >
+      <SafeAreaView
+        className={"flex-row justify-between items-center mx-4 z-10 " + verticalMargin}
+      >
+
+        <TouchableOpacity style={styles.background} className="rounded-xl p-1" onPress={() => navigation.goBack()}>
+          <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => toggleFavourite(!isFavourite)}>
+          <HeartIcon size="35" color={isFavourite ? 'red' : 'white'} />
+        </TouchableOpacity>
+
+      </SafeAreaView>
+
+      {/* person details */}
+      <View>
+        <View
+          className="flex-row justify-center"
+          style={{
+            shadowColor: 'gray',
+            shadowRadius: 40,
+            shadowOffset: { width: 0, height: 5 },
+            shadowOpacity: 1,
+          }}
+        >
+          <View
+            className="items-center rounded-full overflow-hidden h-72 w-72 border-neutral-500 border-2">
+            <Image
+              source={require('../assets/imgs/poster.jpg')}
+              style={{ height: height * 0.43, width: width * 0.74 }}
+            />
+          </View>
+        </View>
+
+        <View className="mt-6">
+          <Text className="text-3xl text-white font-bold text-center">
+            Keanu Reeves
+          </Text>
+          <Text className="text-neutral-500 text-base text-center">
+            Beirut, Lebanon
+          </Text>
+        </View>
+
+        <View className="mx-3 p-4 mt-6 flex-row justify-between items-center bg-neutral-700 rounded-full ">
+          <View className="border-r-2 border-r-neutral-400 px-2 items-center">
+            <Text className="text-white font-semibold ">Gender</Text>
+            <Text className="text-neutral-300 text-sm">
+              Male
+            </Text>
+          </View>
+          <View className="border-r-2 border-r-neutral-400 px-2 items-center">
+            <Text className="text-white font-semibold">Birthday</Text>
+            <Text className="text-neutral-300 text-sm">
+              1964-09-02
+            </Text>
+          </View>
+          <View className="border-r-2 border-r-neutral-400 px-2 items-center">
+            <Text className="text-white font-semibold">known for</Text>
+            <Text className="text-neutral-300 text-sm">
+              Acting
+            </Text>
+          </View>
+          <View className="px-2 items-center">
+            <Text className="text-white font-semibold">Popularity</Text>
+            <Text className="text-neutral-300 text-sm">
+              84.23 %
+            </Text>
+          </View>
+
+        </View>
+        <View className="my-6 mx-4 space-y-2">
+          <Text className="text-white text-lg">Biography</Text>
+          <Text className="text-neutral-400 tracking-wide">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </Text>
+        </View>
+
+        {/* person movies */}
+        <MovieList title="Movies" hideSeeAll={true} data={personMovies} />
+
+      </View>
+    </ScrollView>
+  )
+}
+
+export default PersonScreen
